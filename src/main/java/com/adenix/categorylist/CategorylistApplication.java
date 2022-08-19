26d087af3ab73fafc31ffb58147ec5958ba20de4
Adenicio Batista Freire <adenicio@gmail.com>
2022-08-19 14:14:53 -0300

    Initial commit

---
+++ src/main/java/com/adenix/categorylist/CategorylistApplication.java
@@ -0,0 +1,13 @@
+package com.adenix.categorylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adenix/categorylist/CategorylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adenix.categorylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategorylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
